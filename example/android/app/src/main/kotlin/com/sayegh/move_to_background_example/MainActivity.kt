787e636b4caa999a676fefaf33ad065aceac3198
ViliusP <viliuspaliokas@gmail.com>
2025-02-20 16:24:04 +0200

    Update examples android files

--- example/android/app/src/main/kotlin/com/sayegh/move_to_background_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/sayegh/move_to_background_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.sayegh.move_to_background_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
