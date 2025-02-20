6cd43ae852f85f3a3dc4b6baed3593e9a5e99ea1
ViliusP <viliuspaliokas@gmail.com>
2025-02-20 16:35:38 +0200

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,24 +13,24 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false,
-      onPopInvokedWithResult: (didPop, result) async {
-        MoveToBackground.moveTaskToBack();
-        return;
-      },
-      child: MaterialApp(
-        home: Scaffold(
-          floatingActionButton: FloatingActionButton(
-            child: const Icon(Icons.arrow_downward),
-            onPressed: () {
-              MoveToBackground.moveTaskToBack();
-            },
-          ),
-          appBar: AppBar(
-            title: const Text('MoveToBackground Example'),
-          ),
-          body: const Center(
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.arrow_downward),
+          onPressed: () {
+            MoveToBackground.moveTaskToBack();
+          },
+        ),
+        appBar: AppBar(
+          title: const Text('MoveToBackground Example'),
+        ),
+        body: PopScope(
+          canPop: false,
+          onPopInvokedWithResult: (didPop, result) async {
+            MoveToBackground.moveTaskToBack();
+            return;
+          },
+          child: const Center(
             child: Text('Press the floating action button'),
           ),
         ),
